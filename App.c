e78c8bb27e817445b05d2f11f950be4ec8495c2d
Micahaha <xicer029@gmail.com>
2024-02-08 10:29:56 -0500

    282024

--- App.c
+++ App.c
@@ -1,5 +1,20 @@
 // pre-processor directives
 
 // include needed C and header files
+#include "Assignment.c"
 
 // write code for main function
+
+int main(void){
+
+    /* Pointer Version */
+
+    printf("Intensity        Target Heart Rate\n");
+    heartRateReference(&age, &restingPulse);
+
+
+    /* Normal Version */
+
+    // printf("Intensity        Target Heart Rate\n");
+    // heartRate(&age, &restingPulse);
+}
--- Assignment.c
+++ Assignment.c
@@ -1,5 +1,42 @@
 // pre-processor directives
 
 // include needed header file
+#include "./headers/Assignment.h"
 
 // write code for functions declared in header file
+
+
+void heartRate (int age, int restingPulse){
+
+    float i = START;
+    float print_intensity = i * 100;
+    float targetHeartRate;
+
+    do 
+    {
+        targetHeartRate = (((220 - age) - restingPulse) * i) + restingPulse;
+        printf("%.0f               %.0f bpm\n", print_intensity, targetHeartRate);  
+
+        i += .05;
+        print_intensity = i * 100;
+    } 
+    while(STOP(i));
+    
+}
+
+void heartRateReference (int *age, int *restingPulse){
+    
+    float i = START;
+    float print_intensity = i * 100;
+    float targetHeartRate;
+
+    do 
+    {
+        targetHeartRate = (((220 - *age) - *restingPulse) * i) + *restingPulse;
+        printf("%.0f               %.0f bpm\n", print_intensity, targetHeartRate);  
+
+        i += .05;
+        print_intensity = i * 100;
+    } 
+    while(STOP(i));
+}
